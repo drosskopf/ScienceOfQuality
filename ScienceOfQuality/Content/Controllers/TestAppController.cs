b5a0f1074796a7aee29b1fda12e6e695665f49c3
drosskopf <drosskopf@ldschurch.org>
2015-07-17 05:37:31 -0600

    InitialScienceOfQualityySite

---
+++ ScienceOfQuality/Content/Controllers/TestAppController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScienceOfQuality.Controllers
+{
+    public class TestAppController : Controller
+    {
+        // GET: TestApp
+        public ActionResult CreateTicket()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
